1d9a52ec1d0c0bb470f4ef2c58709cf8b352fe50
Allen <648384410li@gmail.com>
2020-01-29 14:43:29 +0800

    Updates

---
+++ precache-manifest.30fc88c195acc53870733c14e7996673.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fake-zhihu/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e9f20a15d6728cc8e88789fa82486def",
+    "url": "/fake-zhihu/static/media/eyeOpen.e9f20a15.svg"
+  },
+  {
+    "revision": "f12c73a1be4b7e7e544da2f5428eaf06",
+    "url": "/fake-zhihu/static/media/eyeClose.f12c73a1.svg"
+  },
+  {
+    "revision": "c7feca6788cb26946456526a19dbb144",
+    "url": "/fake-zhihu/static/media/UpDownArrow.c7feca67.svg"
+  },
+  {
+    "revision": "01cd9dfec19eac60d5e5",
+    "url": "/fake-zhihu/static/js/runtime~main.01cd9dfe.js"
+  },
+  {
+    "revision": "1eb93c846c2bb9b9f9e4",
+    "url": "/fake-zhihu/static/js/main.1eb93c84.chunk.js"
+  },
+  {
+    "revision": "2f8010cd92ad30f3da41",
+    "url": "/fake-zhihu/static/js/2.2f8010cd.chunk.js"
+  },
+  {
+    "revision": "9e9e2dfacbb1c84326a999fcc1a5c251",
+    "url": "/fake-zhihu/index.html"
+  }
+];
